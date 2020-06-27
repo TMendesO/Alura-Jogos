c78465ba72d2d93c9f72b9fa8603286c83354df5
Thiago Mendes <37598914+ThiagoMendes1@users.noreply.github.com>
2020-06-26 21:20:49 -0300

    Revert "Revert "aula-04""
    This reverts commit d0d014e73f3354d1eebcdb5e1d8262860300b589.

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,50 +1,53 @@
-class Personagem extends Animacao{
+class Personagem extends Animacao {
   constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
     super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
     
     this.variacaoY = variacaoY;
-    this.yInicial = height - this.altura - variacaoY;
+    
+    this.yInicial = height - this.altura - this.variacaoY;
     this.y = this.yInicial;
+    
     this.velocidadeDoPulo = 0;
-    this.gravidade = 6;
-    this.alturaJump = -50;
-    this.jump = 0;
+    this.gravidade = 3;
+    this.alturaDoPulo = -30;
+    this.pulos = 0;
   }
   
   pula(){
-    if(this.jump < 3){
-      this.velocidadeDoPulo = this.alturaJump;
-      this.jump++;
-    }
-  }  
-  
-  aplicaGravidade(){
-    this.y = this.y + this.velocidadeDoPulo;
-    this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade;
-    
-    if(this.y > this.yInicial){
-      this.y = this.yInicial;
-      this.jump = 0;
+    if (this.pulos < 2) {
+      //this.velocidadeDoPulo = this.velocidadeDoPulo - 50;
+      this.velocidadeDoPulo = this.alturaDoPulo;
+      this.pulos++;
     }
   }
   
-  estaColidindo(inimigo){
- //   noFill();
- //   rect(this.x, this.y, this.largura, this.altura);
- //   rect(inimigo.x, inimigo.y, inimigo.largura, inimigo.altura);
+  aplicaGravidade() {
+    this.y = this.y + this.velocidadeDoPulo;
+    this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade;
     
-    const precisao = .7
+    //Momento em que a personagem toca o chão.
+    if (this.y > this.yInicial) {
+      this.y = this.yInicial;
+      this.pulos = 0;
+    }
+  }
+  
+  estaColidindo(inimigo) {
+    //noFill();
+    //rect(this.x, this.y, this.largura, this.altura);
+    //rect(inimigo.x, inimigo.y, inimigo.largura, inimigo.altura);
+    const precisao = 0.65;
     const colisao = collideRectRect(
-      this.x, 
-      this.y, 
-      this.largura * precisao, 
-      this.altura * precisao, 
+      this.x,
+      this.y,
+      this.largura * precisao,
+      this.altura * precisao,
       inimigo.x,
       inimigo.y,
       inimigo.largura * precisao,
       inimigo.altura * precisao
     );
     return colisao;
+    //return false;
   }
-  
 }
