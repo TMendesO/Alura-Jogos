4d4bcb17480bb313640d0a380c10efb66435c3b1
Thiago Mendes <37598914+ThiagoMendes1@users.noreply.github.com>
2020-06-27 06:48:41 -0300

    aula- 04-erro
    corrigi algumas funções mas tem algum erro de logica jogo não incia

--- scripts/jogo/inimigo.js
+++ scripts/jogo/inimigo.js
@@ -1,32 +1,23 @@
 class Inimigo extends Animacao {
-<<<<<<< HEAD
-    constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
-        super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);
-        this.velocida = 5;
-    }
-    move() {
-        this.x = this.x - this.velocida;
+	constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite, velocidade) {
+		super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite)
 
-        if (this.x < -this.largura) {
-            this.x = width;
-        }
-    }
+		this.velocidade = velocidade
+
+		this.x = width
+	}
+
+	move() {
+		this.x = this.x - this.velocidade
+
+
+	}
+
+	aparece() {
+		this.x = width
+	}
+
+	alteraVelocidade(novaVelocidade) {
+		this.velocidade = novaVelocidade
+	}
 }
-=======
-  constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite, velocidade, delay){
-    super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
-    
-    this.velocidade = velocidade;
-    this.delay = delay;
-    this.x = width + this.delay;
-  }  
-  
-  move(){
-    this.x = this.x - this.velocidade;
-    
-    if(this.x < -this.largura - this.delay){
-      this.x = width;
-    }  
-  }
-}  
->>>>>>> parent of ba41b22... aula-04
--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,89 +1,59 @@
-<<<<<<< HEAD
 class Personagem extends Animacao {
-	constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
-		super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);
-		this.yInicial = height - this.altura;
+	constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
+		super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
+		this.variacaoY = variacaoY;
+		this.yInicial = height - this.altura - this.variacaoY;
 		this.y = this.yInicial;
-		this.gravidade = 3;
-		this.velocidadeDoPulo = 0;
 
+		this.velocidadeDoPulo = 0;
+		this.gravidade = 6;
+		this.alturaDoPulo = -50
+		this.pulos = 0
+		this.invencivel = false
 	}
+
 	pula() {
-		this.velocidadeDoPulo = -29;
+		if (this.pulos < 2) {
+			this.velocidadeDoPulo = this.alturaDoPulo
+			this.pulos++
+		}
 	}
 
 	aplicaGravidade() {
-		this.y = this.y + this.velocidadeDoPulo;
-		this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade;
+		this.y = this.y + this.velocidadeDoPulo
+		this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade
 
 		if (this.y > this.yInicial) {
-			this.y = this.yInicial;
-
+			this.y = this.yInicial
+			this.pulos = 0
 		}
+	}
+
+	tornarInvencivel() {
+		this.invencivel = true
+		setTimeout(() => {
+			this.invencivel = false
+		}, 1000)
 	}
 
 	estaColidindo(inimigo) {
-		const precisao = .7;
+		if (this.invencivel) {
+			return false
+		}
 
-		const colisao = collideRectRect(this.x, this.y, this.largura * precisao, this.altura * precisao, inimigo.x, inimigo.y, inimigo.largura * precisao, inimigo.altura * precisao)
+		const precisao = .7
+		const colisao = collideRectRect(
+			this.x,
+			this.y,
+			this.largura * precisao,
+			this.altura * precisao,
+			inimigo.x,
+			inimigo.y,
+			inimigo.largura * precisao,
+			inimigo.altura * precisao
+		);
 
 		return colisao;
-		if (colisao = true) {
-			var img = document.createElement("PNG");
-			img.src = "imagens/assets/game-over.png";
-		}
 	}
 
 }
-=======
-class Personagem extends Animacao{
-  constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
-    super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
-    
-    this.variacaoY = variacaoY;
-    this.yInicial = height - this.altura - variacaoY;
-    this.y = this.yInicial;
-    this.velocidadeDoPulo = 0;
-    this.gravidade = 6;
-    this.alturaJump = -50;
-    this.jump = 0;
-  }
-  
-  pula(){
-    if(this.jump < 3){
-      this.velocidadeDoPulo = this.alturaJump;
-      this.jump++;
-    }
-  }  
-  
-  aplicaGravidade(){
-    this.y = this.y + this.velocidadeDoPulo;
-    this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade;
-    
-    if(this.y > this.yInicial){
-      this.y = this.yInicial;
-      this.jump = 0;
-    }
-  }
-  
-  estaColidindo(inimigo){
- //   noFill();
- //   rect(this.x, this.y, this.largura, this.altura);
- //   rect(inimigo.x, inimigo.y, inimigo.largura, inimigo.altura);
-    
-    const precisao = .7
-    const colisao = collideRectRect(
-      this.x, 
-      this.y, 
-      this.largura * precisao, 
-      this.altura * precisao, 
-      inimigo.x,
-      inimigo.y,
-      inimigo.largura * precisao,
-      inimigo.altura * precisao
-    );
-    return colisao;
-  }
-  
-}
->>>>>>> parent of ba41b22... aula-04
