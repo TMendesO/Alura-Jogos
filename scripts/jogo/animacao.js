571c8e4265eb96f777c1be4bd0dd42db6211f5b5
Thiago Mendes <37598914+ThiagoMendes1@users.noreply.github.com>
2020-06-26 21:22:45 -0300

    Revert "aula-04"
    This reverts commit ba41b2217b5117f0eda2659a86bdd00f9cbea807.

--- scripts/jogo/animacao.js
+++ scripts/jogo/animacao.js
@@ -1,5 +1,6 @@
 class Animacao {
-  constructor (matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
+  constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite){
+    
     this.matriz = matriz;
     this.imagem = imagem;
     this.largura = largura;
@@ -11,19 +12,19 @@ class Animacao {
     this.alturaSprite = alturaSprite;
     
     this.frameAtual = 0;
-  }
+  }  
   
-  exibe() {
+  exibe(){
     image(this.imagem, this.x, this.y, this.largura, this.altura, this.matriz[this.frameAtual][0], this.matriz[this.frameAtual][1], this.larguraSprite, this.alturaSprite);
     
     this.anima();
   }
   
-  anima() {
+  anima(){
     this.frameAtual++;
     
-    if (this.frameAtual >= this.matriz.length - 1) {
+    if(this.frameAtual >= this.matriz.length -1) {
       this.frameAtual = 0;
     }
-  }
+  }  
 }
--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,53 +1,50 @@
-class Personagem extends Animacao {
+class Personagem extends Animacao{
   constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
     super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
     
     this.variacaoY = variacaoY;
-    
-    this.yInicial = height - this.altura - this.variacaoY;
+    this.yInicial = height - this.altura - variacaoY;
     this.y = this.yInicial;
-    
     this.velocidadeDoPulo = 0;
-    this.gravidade = 3;
-    this.alturaDoPulo = -30;
-    this.pulos = 0;
+    this.gravidade = 6;
+    this.alturaJump = -50;
+    this.jump = 0;
   }
   
   pula(){
-    if (this.pulos < 2) {
-      //this.velocidadeDoPulo = this.velocidadeDoPulo - 50;
-      this.velocidadeDoPulo = this.alturaDoPulo;
-      this.pulos++;
+    if(this.jump < 3){
+      this.velocidadeDoPulo = this.alturaJump;
+      this.jump++;
     }
-  }
+  }  
   
-  aplicaGravidade() {
+  aplicaGravidade(){
     this.y = this.y + this.velocidadeDoPulo;
     this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade;
     
-    //Momento em que a personagem toca o chão.
-    if (this.y > this.yInicial) {
+    if(this.y > this.yInicial){
       this.y = this.yInicial;
-      this.pulos = 0;
+      this.jump = 0;
     }
   }
   
-  estaColidindo(inimigo) {
-    //noFill();
-    //rect(this.x, this.y, this.largura, this.altura);
-    //rect(inimigo.x, inimigo.y, inimigo.largura, inimigo.altura);
-    const precisao = 0.65;
+  estaColidindo(inimigo){
+ //   noFill();
+ //   rect(this.x, this.y, this.largura, this.altura);
+ //   rect(inimigo.x, inimigo.y, inimigo.largura, inimigo.altura);
+    
+    const precisao = .7
     const colisao = collideRectRect(
-      this.x,
-      this.y,
-      this.largura * precisao,
-      this.altura * precisao,
+      this.x, 
+      this.y, 
+      this.largura * precisao, 
+      this.altura * precisao, 
       inimigo.x,
       inimigo.y,
       inimigo.largura * precisao,
       inimigo.altura * precisao
     );
     return colisao;
-    //return false;
   }
+  
 }
