c04af37cd4c184bb8abdcb6ce789f5fffda15bfa
Thiago Mendes <37598914+ThiagoMendes1@users.noreply.github.com>
2020-06-26 21:34:41 -0300

    erro
    revert comit git

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Personagem extends Animacao {
 	constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
 		super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);
@@ -34,3 +35,55 @@ class Personagem extends Animacao {
 	}
 
 }
+=======
+class Personagem extends Animacao{
+  constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
+    super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
+    
+    this.variacaoY = variacaoY;
+    this.yInicial = height - this.altura - variacaoY;
+    this.y = this.yInicial;
+    this.velocidadeDoPulo = 0;
+    this.gravidade = 6;
+    this.alturaJump = -50;
+    this.jump = 0;
+  }
+  
+  pula(){
+    if(this.jump < 3){
+      this.velocidadeDoPulo = this.alturaJump;
+      this.jump++;
+    }
+  }  
+  
+  aplicaGravidade(){
+    this.y = this.y + this.velocidadeDoPulo;
+    this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade;
+    
+    if(this.y > this.yInicial){
+      this.y = this.yInicial;
+      this.jump = 0;
+    }
+  }
+  
+  estaColidindo(inimigo){
+ //   noFill();
+ //   rect(this.x, this.y, this.largura, this.altura);
+ //   rect(inimigo.x, inimigo.y, inimigo.largura, inimigo.altura);
+    
+    const precisao = .7
+    const colisao = collideRectRect(
+      this.x, 
+      this.y, 
+      this.largura * precisao, 
+      this.altura * precisao, 
+      inimigo.x,
+      inimigo.y,
+      inimigo.largura * precisao,
+      inimigo.altura * precisao
+    );
+    return colisao;
+  }
+  
+}
+>>>>>>> parent of ba41b22... aula-04
